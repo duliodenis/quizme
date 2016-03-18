2b4fab5bdaedd0e962b138c06c7d8432a8d2f088
Dulio Denis <dulio.denis@yahoo.com>
2016-03-18 11:11:38 -0400

    AnimationEngine on constraints to bounce objects in from offscreen

--- QuizMe/QuizMe/LoginVC.swift
+++ QuizMe/QuizMe/LoginVC.swift
@@ -10,6 +10,23 @@ import UIKit
 import pop
 
 class LoginVC: UIViewController {
-
+    // IBOutlets to Constraints
+    @IBOutlet weak var emailConstraint: NSLayoutConstraint!
+    @IBOutlet weak var passwordConstraint: NSLayoutConstraint!
+    @IBOutlet weak var loginConstraint: NSLayoutConstraint!
+    
+    var animationEngine: AnimationEngine!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        animationEngine = AnimationEngine(constraints: [emailConstraint, passwordConstraint, loginConstraint])
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animationEngine.animateOnScreen(1)
+    }
 }
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # QuizMe
 An iOS 9 / Swift 2 App that uses the Facebook Pop animation engine to present a challenging quiz app.
 
-![](/art/screenshot/quizme1.gif?raw=true)
+![](/art/screenshot/quizme2.gif?raw=true)
 
 ### Licensing
 QuizMe is licensed under [the MIT License](https://github.com/duliodenis/quizme/blob/master/LICENSE).  
